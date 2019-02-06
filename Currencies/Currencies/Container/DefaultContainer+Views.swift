27a2b18f196263aad13aa87e4b3faf9069451d48
Leonardo Piovezan <leonardoapiovezan@gmail.com>
2019-02-05 22:40:58 -0200

    Create Swinject Container Structure

---
+++ Currencies/Currencies/Container/DefaultContainer+Views.swift
@@ -0,0 +1,13 @@
+//
+//  DefaultContainer+Views.swift
+//  Currencies
+//
+//  Created by Leonardo Piovezan on 05/02/19.
+//  Copyright © 2019 Leonardo Piovezan. All rights reserved.
+//
+
+extension DefaultContainer {
+  func registerViews() {
+    
+  }
+}
---
+++ Currencies/Currencies/Container/DefaultContainer.swift
@@ -0,0 +1,22 @@
+//
+//  DefaultContainer.swift
+//  Currencies
+//
+//  Created by Leonardo Piovezan on 05/02/19.
+//  Copyright © 2019 Leonardo Piovezan. All rights reserved.
+//
+
+import Foundation
+import Swinject
+
+final class DefaultContainer {
+  let container: Container
+
+  init() {
+    self.container = Container()
+    self.registerServices()
+    self.registerRepositories()
+    self.registerViews()
+  }
+}
+

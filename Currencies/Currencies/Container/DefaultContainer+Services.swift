8a1ac0a5273c785b18f67ca3e0f3c8b1cc3d555c
Leonardo Piovezan <leonardoapiovezan@gmail.com>
2019-02-05 22:40:58 -0200

    Create Swinject Container Structure

---
+++ Currencies/Currencies/Container/DefaultContainer+Services.swift
@@ -0,0 +1,13 @@
+//
+//  DefaultContainer+Services.swift
+//  Currencies
+//
+//  Created by Leonardo Piovezan on 05/02/19.
+//  Copyright © 2019 Leonardo Piovezan. All rights reserved.
+//
+
+extension DefaultContainer {
+  func registerServices() {
+    
+  }
+}

8d7d19a2fc091b0fa3758eecd4890e5c0510aba8
Leonardo Piovezan <leonardoapiovezan@gmail.com>
2019-02-10 22:19:42 -0200

    Add Structure to Build Elements

---
+++ Currencies/Currencies/Components/TableViewCell/ExchangeRateCell/ExchangeRateCell.swift
@@ -0,0 +1,24 @@
+//
+//  ExchangeRateCell.swift
+//  Currencies
+//
+//  Created by Leonardo Piovezan on 10/02/19.
+//  Copyright © 2019 Leonardo Piovezan. All rights reserved.
+//
+
+import UIKit
+
+class ExchangeRateCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Currencies/CurrenciesTests/Mocks/Services/MockExchangeRateService.swift
@@ -0,0 +1,9 @@
+//
+//  MockExchangeRateService.swift
+//  CurrenciesTests
+//
+//  Created by Leonardo Piovezan on 10/02/19.
+//  Copyright © 2019 Leonardo Piovezan. All rights reserved.
+//
+
+import Foundation

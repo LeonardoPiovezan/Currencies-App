5bafe449c30e09dd33edbbf0c89fd0c709d99178
Leonardo Piovezan <leonardoapiovezan@gmail.com>
2019-02-10 22:21:57 -0200

    Update main modules(View,Screen, ViewModel)

--- Currencies/Currencies/Modules/ExchangeRatesView/ExchangeRatesViewModel.swift
+++ Currencies/Currencies/Modules/ExchangeRatesView/ExchangeRatesViewModel.swift
@@ -7,3 +7,33 @@
 //
 
 import Foundation
+
+final class ExchangeRatesViewModel {
+
+    private let exchangeRateService: ExchangeRatesService
+    var didReceiveRates: (() -> Void)?
+    var failedToReceiveRates: ((Error) -> Void)?
+
+    private (set) var rateResult: RateResult?
+
+    private (set) var rates: [Rate] = []
+
+    init(exchangeRateService: ExchangeRatesService) {
+        self.exchangeRateService = exchangeRateService
+    }
+
+    func updateRatesFor(countryCode: String) {
+        self.exchangeRateService.getRatesFor(countryCode: countryCode) { [weak self] result in
+            switch result {
+            case .success(let rateResult):
+                self?.rateResult = rateResult
+                self?.rates = rateResult.getRateList().sorted(by: { $1.countryCode > $0.countryCode })
+                self?.didReceiveRates?()
+            case .failure(let error):
+                print(error)
+                self?.failedToReceiveRates?(error)
+            }
+        }
+    }
+
+}

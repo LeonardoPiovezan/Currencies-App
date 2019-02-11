0a6dc8a1660a112de4ebe1cdbb157f30a9f16321
Leonardo Piovezan <leonardoapiovezan@gmail.com>
2019-02-10 22:20:16 -0200

    Add RatesViewModelSpec with initial tests

--- Currencies/CurrenciesTests/Modules/ExchangeRatesView/ExchangeRatesViewModelSpec.swift
+++ Currencies/CurrenciesTests/Modules/ExchangeRatesView/ExchangeRatesViewModelSpec.swift
@@ -6,4 +6,56 @@
 //  Copyright © 2019 Leonardo Piovezan. All rights reserved.
 //
 
-import Foundation
+import Quick
+import Nimble
+import Result
+
+@testable import Currencies
+class ExchangeRatesViewModelSpec: QuickSpec {
+
+    var subject: ExchangeRatesViewModel!
+    var ratesService: ExchangeRatesService!
+
+    override func spec() {
+
+        describe("Exchange Rates View Behaviour") {
+
+            context("Received Rates From Service") {
+
+                beforeEach {
+                    let result = Result<RateResult, NetworkError>.success(RateResult(base: "EUR", date: "10-02-2019", rates: ["EUR": 1.0, "BRL": 4.0, "USD": 1.5]))
+                    self.ratesService = MockExchangeRateService(ratesResult: result)
+                    self.subject = ExchangeRatesViewModel(exchangeRateService: self.ratesService)
+                }
+
+                it("Check Condition When result is success with one Rate") {
+                    self.subject.updateRatesFor(countryCode: "EUR")
+                    let result = self.subject.rates
+
+                    let countryCodes = [Rate(countryCode: "BRL", rate: 4.0),
+                                        Rate(countryCode: "EUR", rate: 1.0),
+                                        Rate(countryCode: "USD", rate: 1.5)]
+                    expect(result).to(equal(countryCodes))
+
+                }
+
+            }
+
+            context("Check Condition For Erro from Service") {
+                beforeEach {
+                    let result = Result<RateResult, NetworkError>.failure(NetworkError(message: "Failed to Download"))
+                    self.ratesService = MockExchangeRateService(ratesResult: result)
+                    self.subject = ExchangeRatesViewModel(exchangeRateService: self.ratesService)
+                }
+
+                it("Check if error message is not nil") {
+                    self.subject.failedToReceiveRates = { error in
+                        let networkError = error as! NetworkError
+                    expect(networkError.localizedDescription).to(equal("Failed to Download"))
+                    }
+                    self.subject.updateRatesFor(countryCode: "country")
+                }
+            }
+        }
+    }
+}

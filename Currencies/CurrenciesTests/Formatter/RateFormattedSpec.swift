e19435298846cf29bd36492959e4dd18ea46055e
Leonardo Piovezan <leonardoapiovezan@gmail.com>
2019-02-20 00:24:49 -0300

    Add RateFormatted unit tests

--- Currencies/CurrenciesTests/Formatter/RateFormattedSpec.swift
+++ Currencies/CurrenciesTests/Formatter/RateFormattedSpec.swift
@@ -13,21 +13,42 @@ import Nimble
 
 class RateFormattedSpec: QuickSpec {
     override func spec() {
+        var rate: Rate!
+        var rateFormatted: RateFormatted!
         describe("RateFormatted Behaviour") {
-
-            context("") {
-                it("") {
-
-                    let rate = Rate(currencyCode: "EUR", rate: 1.0)
-
+            context("Rate Has All Fields") {
+                beforeEach {
+                    rate = Rate(currencyCode: "EUR", rate: 2.0)
                     let currencyNameManager = CurrencyNameManagerImpl()
-let rateFormatted = RateFormatted(rate: rate, currencyNameManager: currencyNameManager)
+                    rateFormatted = RateFormatted(rate: rate,
+                                                      currencyNameManager: currencyNameManager,
+                                                      countryFlagsManager: CountryFlagsManagerImpl())
+                }
 
-                    expect(rateFormatted.rateAmount) == 1
+                it("Check If Fields Are Correctly Transformed") {
+                    expect(rateFormatted.rateAmount) == 2
                     expect(rateFormatted.currencyName) == "Euro"
                     expect(rateFormatted.currencyCode) == "EUR"
                     expect(rateFormatted.finalAmount) == ""
-                    expect(rateFormatted.countryImage) != nil
+                }
+
+                it("Check Updating With Typed Amount") {
+                    let newRateFormatted = rateFormatted.updateWith(currentAmount: 2.0)
+                    expect(newRateFormatted.rateAmount) == 2
+                    expect(newRateFormatted.currencyName) == "Euro"
+                    expect(newRateFormatted.currencyCode) == "EUR"
+                    expect(newRateFormatted.finalAmount) == "4.00"
+                }
+            }
+
+            context("Check When Country Is Not Recognized") {
+                it("Check If PlaceholderImage Is Set") {
+                    rate = Rate(currencyCode: "AAAA", rate: 2.0)
+                    let currencyNameManager = CurrencyNameManagerImpl()
+                    rateFormatted = RateFormatted(rate: rate,
+                                                  currencyNameManager: currencyNameManager,
+                                                  countryFlagsManager: CountryFlagsManagerImpl())
+                    expect(rateFormatted.countryImage) == UIImage(named: "flagPlaceholder")
                 }
             }
         }
